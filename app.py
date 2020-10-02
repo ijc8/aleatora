8c88379bfeb6f84c121574de7aed92c7a8bd7b16
Ian Clester <ijc@mit.edu>
2020-10-02 08:44:27 -0400

    Draw stream graph next to code.

--- app.py
+++ app.py
@@ -4,10 +4,24 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.codeinput import CodeInput
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.scatter import Scatter, ScatterPlane
 
 import next
 import audio
 import graph
+
+class GraphLabel(Label):
+    pass
+
+class GraphTitleLabel(Label):
+    pass
+
+class GraphLayout(BoxLayout):
+    pass
+
+class GraphBorderLayout(GraphLayout):
+    pass
 
 class CustomInput(CodeInput):
     def __init__(self, run_button):
@@ -23,6 +37,34 @@ class CustomInput(CodeInput):
         return super().keyboard_on_key_down(window, keycode, text, modifiers)
 
 
+def build_graph_layout(stream):
+    def dfs(node):
+        name = ''
+        if isinstance(node[0], int):
+            name = f'{node[0]}:'
+            node.pop(0)
+        name += node[0]
+        if node[0].startswith('Stream'):
+            return GraphLabel(text=name)
+        elif any(node[0].startswith(s) for s in ('Map', 'Slice')):
+            box = GraphBorderLayout(orientation='vertical')
+            box.add_widget(GraphTitleLabel(text=name))
+            box.add_widget(dfs(node[2]))
+            return box
+        elif any(node[0].startswith(s) for s in ('Concat', 'Mix', 'Zip')):
+            box = GraphBorderLayout(orientation='vertical')
+            box.add_widget(GraphTitleLabel(text=name))
+            collection = GraphLayout(orientation='horizontal' if node[0].startswith('Concat') else 'vertical')
+            print('going in', len(node[1]))
+            for child in node[1]:
+                collection.add_widget(dfs(child))
+            box.add_widget(collection)
+            return box
+        elif node[0] == 'cycle!':
+            return GraphLabel(text=f'Ref {node[1]}')
+        return name
+    return dfs(graph.traverse(stream))
+
 
 class MyApp(App):
     def run_code(self, button):
@@ -34,19 +76,29 @@ class MyApp(App):
             else:
                 main = globals['main']
                 graph.text_graph(main)
+                self.graph_layout.clear_widgets()
+                self.graph = build_graph_layout(main)
+                self.graph_layout.add_widget(self.graph)
                 audio.play(main)
         except Exception as e:
             print(e)
 
     def build(self):
-        button = Button(text='Run', size=(0, 50), size_hint=(1, None))
+        button = Button(text='Run', height=40, size_hint=(1, None))
         self.input = CustomInput(button)
         self.input.text = 'main = silence'
         button.bind(on_press=self.run_code)
-        layout = BoxLayout(orientation='vertical')
-        layout.add_widget(self.input)
-        layout.add_widget(button)
-        return layout
+
+        self.layout = BoxLayout(orientation='horizontal')
+        codebox = BoxLayout(orientation='vertical')
+        codebox.add_widget(self.input)
+        codebox.add_widget(button)
+        self.layout.add_widget(codebox)
+        self.graph_layout = Scatter()
+        self.graph = BoxLayout(orientation='vertical')
+        self.graph_layout.add_widget(self.graph)
+        self.layout.add_widget(self.graph_layout)
+        return self.layout
 
 
 if __name__ == '__main__':
