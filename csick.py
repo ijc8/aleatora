715681adbe13c307048971352f7a6f09be1bfd55
Ian Clester <ijc@mit.edu>
2020-10-23 03:43:46 -0400

    Check in csick, add verbose kwarg to wav.save().

---
+++ csick.py
@@ -0,0 +1,51 @@
+from core import *
+from chord import *
+import wav
+
+c = Chord((60, 64, 67, 72))
+m = freeze(basic_sequencer(arp(c)[:4], bpm=20))
+
+# (time, modulation frequency)
+freq_points = [
+    (0.0,  0.0),
+    (15.0, 0.0),
+    (20.0, 0.25),
+    (25.0, 0.5),
+    (30.0, 1.0),
+    (35.0, 2.0),
+    (40.0, 4.0),
+    (45.0, 8.0),
+    (50.0, 16.0),
+    (55.0, 32.0),
+    (56.0, 0.0),
+    (60.0, 0.0)
+]
+
+# (time, modulation depth)
+depth_points = [
+    (0.0,  0.0),
+    (15.0, 0.0),
+    (20.0, 0.01),
+    (25.0, 0.1),
+    (30.0, 0.2),
+    (35.0, 0.3),
+    (40.0, 0.4),
+    (45.0, 0.5),
+    (50.0, 1.0),
+    (55.0, 2.0),
+    (56.0, 0.0),
+    (60.0, 0.0)
+]
+
+mod_freq = freeze(interp(freq_points))
+mod_depth = freeze(interp(depth_points))
+low_rate = 1 + fm_osc(mod_freq) * mod_depth
+mid_rate = 2 + fm_osc(mod_freq) * mod_depth
+high_rate = 3 + fm_osc(mod_freq) * mod_depth
+
+low = resample(cycle(m), low_rate)
+mid = silence[:5.0] >> (resample(cycle(m), mid_rate[5.0:]) * basic_envelope(58.0 - 5.0))
+high = silence[:10.0] >> (resample(cycle(m), high_rate[10.0:]) * basic_envelope(56.0 - 10.0))
+
+composition = (low + mid + high)/3
+wav.save(composition, "csick.wav", verbose=True)
--- wav.py
+++ wav.py
@@ -1,9 +1,12 @@
 import core
-import wave
+
 import numpy as np
 
+import time
+import wave
 
-def save(comp, filename, chunk_size=8192):
+
+def save(comp, filename, chunk_size=16384, verbose=False):
     w = wave.open(filename, 'wb')
     w.setnchannels(1)
     w.setsampwidth(2)
@@ -11,8 +14,14 @@ def save(comp, filename, chunk_size=8192):
     chunk = np.empty(chunk_size, dtype=np.float)
     siter = iter(comp)
     i = chunk_size - 1
+    if verbose:
+        t = 0
+        start_time = time.time()
     while i == chunk_size - 1:
         for i, sample in zip(range(chunk_size), siter):
             chunk[i] = sample
         w.writeframes((chunk[:i] * (2**15-1)).astype(np.int16))
+        if verbose:
+            t += i
+            print(f'{t} ({t/core.SAMPLE_RATE}) - real time: {time.time() - start_time}')
     w.close()
