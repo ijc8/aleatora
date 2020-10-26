1c80ddd7f4c7654c9201e4bf3dba611602fb7e38
Ian Clester <ijc@mit.edu>
2020-10-26 03:44:37 -0400

    Initial work on playing samples and rhythms.

--- wav.py
+++ wav.py
@@ -6,6 +6,28 @@ import time
 import wave
 
 
+def load(filename):
+    with wave.open(filename, 'rb') as w:
+        width = w.getsampwidth()
+        channels = w.getnchannels()
+        buffer = w.readframes(w.getnframes())
+        if width == 2:
+            return np.frombuffer(buffer, dtype=np.int16).reshape(-1, channels).astype(np.float) / np.iinfo(np.int16).max
+        elif width == 3:
+            audio = np.frombuffer(buffer, dtype=np.uint8).reshape(-1, 3)
+            converted = np.bitwise_or.reduce(audio << np.array([8, 16, 24]), dtype=np.int32, axis=1)
+            return converted.astype(np.float).reshape(-1, channels) / np.iinfo(np.int32).max
+        elif width == 4:
+            return np.frombuffer(buffer, dtype=np.int32).astype(np.float).reshape(-1, channels) / np.iinfo(np.int32).max
+        else:
+            raise NotImplementedError(f"{width*8}-bit wave files not supported")
+
+
+def load_mono(filename):
+    audio = load(filename)
+    return audio.sum(axis=1) / audio.shape[1]
+
+
 def save(comp, filename, chunk_size=16384, verbose=False):
     w = wave.open(filename, 'wb')
     w.setnchannels(1)
