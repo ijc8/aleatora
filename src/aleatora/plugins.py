61f4256fc4c846f1b1bfe20233231859dcc22801
Ian Clester <ijc@ijc8.me>
2021-08-17 14:54:43 -0400

    More experimentation with pyvst

--- src/aleatora/plugins.py
+++ src/aleatora/plugins.py
@@ -3,12 +3,39 @@ import pyvst
 
 from .streams import stream, SAMPLE_RATE
 
-def load_vsti(path, block_size=512, volume_threshold=2e-6):
+OPEN_EDITOR = 0 # redacted, just in case
+IDLE_EDITOR = 0 # redacted, just in case
+
+class VSTEditor:
+    def __init__(self, vst):
+        self.vst = vst
+
+    def open(self):
+        self.vst._dispatch(OPEN_EDITOR)
+    
+    def idle(self):
+        self.vst._dispatch(IDLE_EDITOR)
+
+class HostedVST:
+    def __init__(self, path, block_size, volume_threshold, instrument):
+        self.block_size = block_size
+        self.volume_threshold = volume_threshold
+        self.instrument = instrument
+        self.host = pyvst.SimpleHost(path, sample_rate=SAMPLE_RATE, block_size=block_size)
+        self.vst = self.host.vst
+        # self.vst.verbose = True
+        self.ui = VSTEditor(self.vst)
+    
+    def __call__(self, stream):
+        if self.instrument:
+            return self.run_with_events(stream)
+        else:
+            raise NotImplementedError
+
     @stream
-    def instrument(event_stream):
-        host = pyvst.SimpleHost(path, sample_rate=SAMPLE_RATE, block_size=block_size)
+    def run_with_events(self, event_stream):
         delta_frames = 0
-        for event_chunk in event_stream.chunk(block_size):
+        for event_chunk in event_stream.chunk(self.block_size):
             vst_events = []
             for events in event_chunk:
                 for event in events:
@@ -16,17 +43,19 @@ def load_vsti(path, block_size=512, volume_threshold=2e-6):
                     vst_events.append(pyvst.midi.midi_note_event(note=int(event.note), velocity=velocity, kind=event.type, delta_frames=delta_frames))
                     delta_frames = 0
                 delta_frames += 1
-            host.vst.process_events(pyvst.midi.wrap_vst_events(vst_events))
+            self.vst.process_events(pyvst.midi.wrap_vst_events(vst_events))
             # TODO: Handle multiple channels.
-            yield from host.vst.process(input=None, sample_frames=block_size)[0, :].tolist()
-            host.transport.step(block_size)
+            yield from self.vst.process(input=None, sample_frames=self.block_size)[0, :].tolist()
+            self.host.transport.step(self.block_size)
         # TODO: Optional end condition, like low volume?
         while True:
-            chunk = host.vst.process(input=None, sample_frames=block_size)[0, :]
+            chunk = self.vst.process(input=None, sample_frames=self.block_size)[0, :]
             yield from chunk.tolist()
-            host.transport.step(block_size)
-            if volume_threshold is not None:
+            self.host.transport.step(self.block_size)
+            if self.volume_threshold is not None:
                 rms = np.sqrt((chunk ** 2).mean())
-                if rms < volume_threshold:
+                if rms < self.volume_threshold:
                     return
-    return instrument
+
+def load_vsti(path, block_size=512, volume_threshold=2e-6):
+    return HostedVST(path, block_size, volume_threshold=volume_threshold, instrument=True)
