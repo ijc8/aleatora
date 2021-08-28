eb54b5cab6ebf687134b68812ee2ef83103b256d
Ian Clester <ijc@ijc8.me>
2021-08-27 22:11:51 -0400

    Autodoc, take 4.

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'Ian Clester'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc']
+autodoc_mock_imports = ['sounddevice']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
